eae768885510320689a8274efda7535b456ed993
eellaup <paullee11760@gmail.com>
2020-01-24 23:54:27 -0600

    Updates

---
+++ precache-manifest.b4408742465adcdbc22bd3be1b7d0f8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b86f72ce905a0bf16459c3450e7bc7fc",
+    "url": "/Website/index.html"
+  },
+  {
+    "revision": "e630f072e62bb04ece37",
+    "url": "/Website/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "11485537f282600ec466",
+    "url": "/Website/static/js/2.8baa779a.chunk.js"
+  },
+  {
+    "revision": "c97e37d5488e4d07529e18e084899b03",
+    "url": "/Website/static/js/2.8baa779a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e630f072e62bb04ece37",
+    "url": "/Website/static/js/main.516f74fa.chunk.js"
+  },
+  {
+    "revision": "c38158934700b8edb219",
+    "url": "/Website/static/js/runtime-main.393d5146.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Website/precache-manifest.533404038788a68f0a437bd397aa6f23.js"
+  "/Website/precache-manifest.b4408742465adcdbc22bd3be1b7d0f8e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Website/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
